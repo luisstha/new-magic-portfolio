cc978f2c6722c62f665fb4dae83c9fb380e5d98f
luis <sthaluiz@gmail.com>
2025-11-24 12:03:51 +0545

    fixed

--- src/components/Testimonial.tsx
+++ src/components/Testimonial.tsx
@@ -31,7 +31,7 @@ const Testimonial = () => {
             <iframe 
               width="100%" 
               height="100%" 
-              src="https://youtube.com/shorts/EC61aoVQ6fk?si=DLBxoVowPO_tfKlR" 
+              src="https://www.youtube.com/embed/EC61aoVQ6fk?rel=0" 
               title="Luis Shrestha Performance"
               frameBorder="0" 
               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
